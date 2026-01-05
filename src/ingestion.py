4fca4a4a1879ee12fd59446e688e814b34e5cf7c
ekelenna1 <e.kelenna@wustl.edu>
2026-01-05 00:32:50 -0600

    feat: implement vector storage using Gemini and ChromaDB

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,14 +1,18 @@
 import pytesseract
+import os
 from PIL import Image
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import Chroma
+
+load_dotenv()
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def extract_text_from_image(image_path):
-    """
-    Opens an image, converts to grayscale, and extracts text.
-    """
+    """Opens an image, converts to grayscale, and extracts text."""
     img = Image.open(image_path).convert('L')
     text = pytesseract.image_to_string(img)
     return text
@@ -22,14 +26,30 @@ def process_legal_document(text_content):
     )
     return splitter.split_text(text_content)
 
+def store_in_vector_db(chunks):
+    """Stores text chunks into vector, and stores in a file."""
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    
+    vector_db = Chroma.from_texts(
+        texts=chunks,
+        embedding=embeddings,
+        persist_directory="./db"
+    )
+    return vector_db
+
 if __name__ == "__main__":
     print("--- Testing Ingestion Pipeline ---")
     target_image = "data/test_dec28.png"
     
     try:
-        result = extract_text_from_image(target_image)
-        print("✅ Success! Extracted Text:\n")
-        print(result)
+        raw_text = extract_text_from_image(target_image)
+        print("✅ OCR Complete.")
+
+        chunks = process_legal_document(raw_text)
+        print(f"✅ Created {len(chunks)} semantic chunks.")
+
+        store_in_vector_db(chunks)
+        print("✅ Database initialized in /db folder.")
+
     except Exception as e:
         print(f"❌ Error: {e}")
-        print(f"\nTip: Ensure the file exists at: {target_image}")
